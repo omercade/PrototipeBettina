9ae94d26629fcaa6f22f1576ecfc6d2a99b9c5b8
omercade <omercade@omitsis.com>
2015-02-11 11:52:07 +0100

    First commit

---
+++ www/nav.js
@@ -0,0 +1,11 @@
+alert(2);
+
+document.write('\
+<a href="index.html" class="active">Über das Museum</a>\
+<a href="#two">Multimedia Tour </a>\
+<a href="#timeline">Timeline</a>\
+<a href="#three">Quizz</a>\
+<a href="#three">Veranstaltungskalender</a>\
+<a href="#three">Service</a>\
+<a href="#four">Contact</a>\			
+');
